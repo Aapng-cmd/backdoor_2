cf3b3bca060d457bf523f01d47471d229cfdbf17
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2024-01-05 23:49:03 +0300

    ...

--- tmp/t.py
+++ tmp/t.py
@@ -1,16 +1,25 @@
-import pyshark, subprocess
+import pyshark, subprocess, base64
 from prettytable import PrettyTable
 answer = PrettyTable()
-answer.field_names = ["Src ip", "Dest ip"]
+answer.field_names = ["Src ip", "Dest ip", "based http_req_data", "server answer"]
+# answer.field_names = ["Src ip", "Dest ip", "server answer"]
 
 
 # set an analyzer for handshakes/ http/s traffic/ password/username searching
 
-capture = pyshark.LiveCapture(interface='Беспроводная сеть', display_filter='tcp')
+capture = pyshark.LiveCapture(interface='Беспроводная сеть', display_filter='http')
 
-for packet in capture.sniff_continuously(packet_count=6):
-    # if "TLS" in packet:
-    print('Just arrived:', packet)
-    answer.add_row([packet.ip.src, packet.ip.dst])
+for packet in capture.sniff_continuously(packet_count=2):
+    server_ans = ""
+    # if "" in packet:
+    # print('Just arrived:', packet)
+    # print(dir(packet.http))
+    # exit()
+    # print(dir(packet))
+    try: server_ans = packet['data-text-lines']
+    except KeyError: pass
+    # print(packet)
+    answer.add_row([packet.ip.src, packet.ip.dst, base64.b64encode(str(packet.http).encode()).decode(), base64.b64encode(":".join(str(server_ans).split(":")[1:]).encode()).decode()])
+    # answer.add_row([packet.ip.src, packet.ip.dst, base64.b64encode(":".join(str(server_ans).split(":")[1:]).encode()).decode()])
 
 print(answer)
