9c4b3f738861432e07b2a5fa64edf83d759a42d8
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2024-01-05 21:44:36 +0300

    ...

--- tmp/t.py
+++ tmp/t.py
@@ -1,10 +1,16 @@
 import pyshark, subprocess
+from prettytable import PrettyTable
+answer = PrettyTable()
+answer.field_names = ["Src ip", "Dest ip"]
 
 
 # set an analyzer for handshakes/ http/s traffic/ password/username searching
 
 capture = pyshark.LiveCapture(interface='Беспроводная сеть', display_filter='tcp')
 
-for packet in capture.sniff_continuously(packet_count=1000):
+for packet in capture.sniff_continuously(packet_count=6):
+    # if "TLS" in packet:
     print('Just arrived:', packet)
+    answer.add_row([packet.ip.src, packet.ip.dst])
 
+print(answer)
